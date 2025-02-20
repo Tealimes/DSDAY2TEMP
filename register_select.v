e969d70b9489a4f7c07665e9548deb5e5dfdcb6d
Alexander Peacock <147108392+Tealimes@users.noreply.github.com>
2025-02-20 16:24:00 -0500

    Update register_select.v

--- register_select.v
+++ register_select.v
@@ -7,17 +7,5 @@ module register_select #(parameter WIDTH = 32)(
     reg[WIDTH-1:0] Reg_list [0:WIDTH-1];
   
       
-   integer i;
-   always@(posedge iClk)begin
-        if(!iRstN)begin
-            for(i = 0; i < WIDTH; i = i + 1)
-                Reg_list[i] <= 32'b0;
-        end
-        
-        Reg_list[RD] = RD_data; //writes from previous output
-   end
-   
-   assign RS1_data = Reg_list[RS1];
-   assign RS2_data = Reg_list[RS2]; 
    
 endmodule
