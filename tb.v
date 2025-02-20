c7fbbd10c2a0b2ff5363c869ecfa675a00aacb1d
Alexander Peacock <147108392+Tealimes@users.noreply.github.com>
2025-02-20 16:25:08 -0500

    Update tb.v

--- tb.v
+++ tb.v
@@ -2,28 +2,5 @@
 
 
 module tb();
-    parameter WIDTH = 32;
-    reg iClk, iRstN; // inputs
-    wire [WIDTH-1:0] rd; // output
-    
-    // module instantiation
-    top CPU (.iClk(iClk),.iRstN(iRstN),.rd(rd));
-    
-    always #5 iClk = ~iClk;
-    
-    initial begin
-        $dumpfile("tb.vcd"); $dumpvars;
-        
-        iClk = 0;
-        iRstN = 0;
-        
-        #10; 
-        
-        iRstN = 1;
-        
-        #1000;
-        
-        $finish;
-    
-    end
+
 endmodule
